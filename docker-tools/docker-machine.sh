b767c8e06b12b3a3801d33f0478a45a928c9f2d9
sumoning <sumoning@live.com>
2018-08-23 14:06:00 +0800

    添加gitlab

---
+++ docker-tools/docker-machine.sh
@@ -0,0 +1,5 @@
+base=https://raw.githubusercontent.com/docker/machine/v0.14.0
+for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
+do
+  sudo wget "$base/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+done
